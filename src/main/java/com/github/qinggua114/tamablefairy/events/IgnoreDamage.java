4b74a1218debddd0ff9ef3fcdba575a68b9317d6
qinggua114 <a305091754@163.com>
2026-02-28 14:15:32 +0800

    Cleaned useless codes

--- src/main/java/com/github/qinggua114/tamablefairy/events/IgnoreDamage.java
+++ src/main/java/com/github/qinggua114/tamablefairy/events/IgnoreDamage.java
@@ -2,12 +2,10 @@ package com.github.qinggua114.tamablefairy.events;
 
 import com.github.qinggua114.tamablefairy.data.TameData;
 import com.github.tartaricacid.touhoulittlemaid.entity.monster.EntityFairy;
-import com.mojang.logging.LogUtils;
 import net.minecraft.world.entity.Entity;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
-import org.slf4j.Logger;
 
 import static com.github.qinggua114.tamablefairy.data.Attachments.TAME_DATA;
 
