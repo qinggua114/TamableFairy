facbd245da9d765923eea2511a05c7740ced76ad
qinggua114 <a305091754@163.com>
2026-03-13 00:27:25 +0800

    修复了一个潜在的NPE

--- src/main/java/com/github/qinggua114/tamablefairy/events/DamageEvents.java
+++ src/main/java/com/github/qinggua114/tamablefairy/events/DamageEvents.java
@@ -24,9 +24,10 @@ public class DamageEvents {
     public static void onTamedFairyDamage(LivingDamageEvent event){
         //如果是同一个主人的女仆妖精,则忽略伤害
         LivingEntity entity = event.getEntity();
-        if(!(entity.getClass().equals(EntityFairy.class)) || entity.level().isClientSide) return;
+        if (!(entity.getClass().equals(EntityFairy.class)) || entity.level().isClientSide) return;
         Entity source = event.getSource().getEntity();
-        if(!(source.getClass().equals(EntityFairy.class))) return;
+        if (source == null) return;
+        if (!(source.getClass().equals(EntityFairy.class))) return;
 
         ITameData tameData = entity.getCapability(TAME_DATA, null).orElse(new TameData());
         if (!tameData.tamed()) return;
